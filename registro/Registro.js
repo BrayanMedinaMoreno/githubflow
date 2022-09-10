4136913263c0a76acfa37412ef6cc2ee42f703f5
BrayanMedinaMoreno <brayan0519@yahoo.com>
2022-09-09 19:01:15 -0500

    primer commit

---
+++ registro/Registro.js
@@ -0,0 +1,10 @@
+//* input
+
+let contenido = document.getElementById('RegistoInputCorreo');
+let contenido2 = document.getElementById('RegistroInputContraseña');
+
+try {
+    const respuesta = await fetch()
+} catch (error) {
+    
+}
